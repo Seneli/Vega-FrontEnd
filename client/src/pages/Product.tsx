33a85a38d2a139c0a2a6ea43fc4313a96600343d
seneli <seneli.sene@gmail.com>
2022-12-29 11:40:00 +0530

    preliminary styling to about and product pages

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
-import overview_content from 'static/text/project-overview';
+import overview_content from 'static/text/product';
+import styled from 'styled-components';
 
 const Product = () => {
   return (
     <>
-      <h1>Gonna be the product overview page</h1>
+      <PageHeader>Product Overview</PageHeader>
       {overview_content.map((content, index) => {
         return (
-          <div>
-            <h2>{content.title}</h2>
-            <p>{content.paragraph}</p>
-          </div>
+          <PageBody>
+            <Subtitle>{content.title}</Subtitle>
+            <Paragraph>{content.paragraph}</Paragraph>
+          </PageBody>
         );
       })}
     </>
   );
 };
+const PageHeader = styled.h1`
+  font-size: 80px;
+  line-height: 84px;
+  /* margin-bottom: 30px; */
+  margin: 0;
+  padding: 100px;
+  font-weight: 700;
+  color: rgb(25, 25, 27);
+  background-image: linear-gradient(
+    147deg,
+    rgba(141, 141, 236, 0.17) 0%,
+    rgba(84, 84, 212, 0) 100%
+  );
+`;
+
+const PageBody = styled.div`
+  padding: 20px 100px;
+`;
+
+const Subtitle = styled.h2`
+  color: ${(props) => props.theme.colors.primaryPink};
+`;
+
+const Paragraph = styled.p``;
 
 export default Product;
