0a1f570a49059c6a8e0adc8256ceeed96c7c3d47
seneli <seneli.sene@gmail.com>
2022-12-29 14:26:55 +0530

    added dashboard mock data

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import styled from 'styled-components';
+import content from 'static/mock/dashboard';
+
+import QuickStats from 'components/QuickStats';
+
+interface TeamProps {
+  members: {
+    name: string;
+    position: string;
+    linkedin: string;
+    picture: any;
+  }[];
+}
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>This is gonna be the dashboard page</h1>
-    </div>
+    <>
+      <PageBody>
+        <SectionHeading>Quick Stats</SectionHeading>
+        <QuickStats stats={content.stats} />
+        {/* {content.stats.map((content, index) => {
+          return (
+            <PageBody>
+              <Subtitle>{content.title}</Subtitle>
+              <Paragraph>{content.paragraph}</Paragraph>
+            </PageBody>
+          );
+        })} */}
+        <DashboardGrid>
+          <SectionHeading>
+            List of Components and Vulnerabilities
+          </SectionHeading>
+        </DashboardGrid>
+      </PageBody>
+    </>
   );
 };
+
+const PageBody = styled.div`
+  padding: 20px 100px 0 100px;
+`;
+
+const SectionHeading = styled.h2``;
+
+const DashboardGrid = styled.div`
+  display: grid;
+  grid-template-areas: '
+  SectionHeading ViewToggle
+  SearchBar OptionButtons
+  Dashboard Dashboard
+  ';
+`;
 
 export default Dashboard;
