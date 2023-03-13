47337d52970556248e84140d0a2804db8f9a1207
z-arnott <zoe.arnott@hotmail.com>
2023-03-13 19:28:53 -0400

    add dynamic session ID

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -46,7 +46,7 @@ const Dashboard = () => {
     // RESEND THE REQUEST FROM THE DASHBOARD EVERY RENDER
     setLoading(true);
     const queryParams = {
-      sessionId: 27,
+      sessionId: sessionStorage.getItem('sessionID'),
       view: view,
       riskFilters: riskFilters,
       severityFilters: impactFilters,
